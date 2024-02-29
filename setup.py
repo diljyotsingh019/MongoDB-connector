a3b3a4cc8f899804d06fde47aecb0e19f007b9ed
diljyotsingh019 <diljyotsingh019@gmail.com>
2024-02-29 11:38:37 -0500

    setup file changes

--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
 from setuptools import setup, find_packages
-from typing import List 
+from typing import List
 
-HYPEN_E_DOT = "-e ."
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()     
+   
+__version__ = "0.0.4"
+REPO_NAME = "MongoDB-connector"
+PKG_NAME= "mongo-connector"
+AUTHOR_USER_NAME = "diljyotsingh019"
+AUTHOR_EMAIL = "diljyotsingh019@gmail.com"
 
-def get_requirements(file_path: str) ->List[str]:
-    with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        requirements = [req.replace("\n", "") for req in requirements]
-
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-
-        return requirements
-    
 setup(
-    name = "DiamondPricePrediction",
-    version = "0.0.1",
-    author = "Diljyot Singh",
-    author_email = "diljyotsingh019@gmail.com",
-    install_requires = ["scikit-learn", "pandas", "numpy"],
-    packages = find_packages()
-)
+    name=PKG_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A python package for connecting with database.",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    )
