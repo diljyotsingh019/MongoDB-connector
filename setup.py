8f1c0674c4a80747a8395593407342cc3a700fc3
diljyotsingh019 <diljyotsingh019@gmail.com>
2024-02-29 22:00:22 -0500

    changes

--- setup.py
+++ setup.py
@@ -1,21 +1,9 @@
 from setuptools import setup, find_packages
 from typing import List
 
-HYPEN_E_DOT = "-e ."
-
-def get_requirements(file_path:str)->List[str]:
-    with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
-
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-
-    return requirements
-
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
-   
+  
 __version__ = "0.0.4"
 REPO_NAME = "MongoDB-connector"
 PKG_NAME= "test-mongo-connector"
@@ -35,5 +23,5 @@ setup(
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
-    packages=find_packages(where="src")
+    packages=find_packages(where="src"),
     )
